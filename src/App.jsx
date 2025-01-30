4131cbc5ea5f41f95306a60f84bdd378ac024d9b
nrusk <nrusk@ivytech.edu>
2025-01-29 20:06:05 -0500

    9th commit

--- src/App.jsx
+++ src/App.jsx
@@ -17,16 +17,63 @@ const App = () => {
     },
     {
       img: image2,
-      name:'Eva Smith',
+      name:'Lily Smith',
       title:'UX Designer',
       email:'b@b.com'
     },
+    {
+      img: image1,
+      name:'Bob Johnson',
+      title:'Web Developer',
+      email:'c@c.com'
+    },
+    {
+      img: image2,
+      name:'Ava Smith',
+      title:'Web Developer',
+      email:'d@d.com'
+    },
+    {
+      img: image1,
+      name:'Tom Smith',
+      title:'Software Engineer',
+      email:'e@e.com'
+    },
+    {
+      img: image2,
+      name:'Eva Smith',
+      title:'Graphic Designer',
+      email:'f@f.com'
+    },
   ];
 
-const [clicked, setClicked] = useState(false);
-const handleClick = () => {
-  setClicked(!clicked);
-};
+  //get titles
+  const titles = [...new Set(profiles.map((profile) => profile.title))];
+
+  const [title, setTitle] = useState("");
+  //update title on change of the dropdown
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const [search, setSearch] = useState("");
+  //update the search on change of input
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  //clear title and search
+  const handleClear = () => {
+    setTitle("");
+    setSearch("");
+  };
+
+  //filter the profiles based on the title
+  const filterProfiles = profiles.filter(
+    (profile) => 
+      (title === "" || profile.title === title) &&
+      profile.name.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <>
@@ -36,19 +83,47 @@ const handleClick = () => {
       <main>
         <Wrapper>
           <h1>Profile App</h1>
-          <button onClick={handleClick}>
-            {clicked ? "Click me" : "Clicked"}
-          </button>
         </Wrapper>
         <Wrapper>
           <About />
         </Wrapper>
         <Wrapper>
+          <div className="filter-wrapper">
+            <div className="filter--select">
+              <label htmlFor="title-select">Select a title:</label>
+              <select
+                id="title-select" 
+                onChange={handleTitleChange}
+                value={title}
+              >
+                <option value="">All</option>
+                {titles.map((title) => (
+                  <option key={title} value={title}>
+                    {title}
+                  </option>
+                ))}
+                {/* <option value="Software Engineer">Software Engineer</option>
+                <option value="Software Developer">Software Developer</option>
+                <option value="Ux Designer">Ux Designer</option>
+                <option value="Graphic Designer">Graphic Designer</option> */}
+              </select>
+            </div>
+            <div className="filter--search">
+              <label htmlFor="search">Search by name:</label>
+              <input
+                type="text"
+                id="search"
+                onChange={handleSearchChange}
+                value={search}
+              />
+            </div>
+                <button onClick={handleClear}>Clear</button>
+          </div>
           <div className="profile-cards">
-            {profiles.map((profile) => (
+            {filterProfiles.map((profile) => (
               <Card key={profile.email} {...profile} />
-              ))}
-        </div>
+            ))}
+          </div>
         </Wrapper>
       </main>
     </>
