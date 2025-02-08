e9daf3059352ba82acc3584ca99d9782fed9cdbf
nrusk <nrusk@ivytech.edu>
2025-02-07 20:51:16 -0500

    13th commit

--- src/components/ProfileForm.jsx
+++ src/components/ProfileForm.jsx
@@ -2,22 +2,59 @@ import { useState } from "react";
 import style from "../styles/ProfileForm.module.css";
 
 const ProfileForm = () => {
-    const [data, setData] = useState({ name: "", title: "", email: "", bio: "" });
+    const [data, setData] = useState({ name: "", title: "", email: "", bio: "", image: null });
+    const [errors, setErrors] = useState({ image: "", general: "" });
+    const [submitting, setSubmitting] = useState(false);
+    const [successMessage, setSuccessMessage] = useState("");
+
     const handleChange = (e) => {
-        setData({ ...data, [e.target.name]: e.target.value });
+        if (e.target.name === "image") {
+            const file = e.target.files[0];
+            if (file.size > 2000000) {
+                setErrors({...errors, image: "Image must be less that 2MB."});
+            } else {
+                setData({ ...data, image: file });
+            }
+            console.log(file);
+            console.log(data.image);
+        } else {
+            setData({ ...data, [e.target.name]: e.target.value });
+        }
+        
     };
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const formData = new FormData(e.target);
+        setSubmitting(true);
+        const formData = new FormData();
+        formData.append("name", data.name.trim());
+        formData.append("email", data.email.trim());
+        formData.append("title", data.title.trim());
+        formData.append("bio", data.bio.trim());
+        if (data.image) formData.append("image", data.image);
+        console.log(data.image+"test");
         try {
             const response = await fetch("https://web.ics.purdue.edu/~nrusk/profile-app/send-data.php", {
                 method: "POST",
                 body: formData,
             });
             const result = await response.json();
-            console.log(result.message);
+            if (result.success) {
+                setData({ name: "", title: "", email: "", bio: "", image: null });
+                setErrors({image: "", general: ""});
+                setSuccessMessage("Data submitted successfully.");
+                setTimeout(() => {
+                    setSuccessMessage("");
+                }, 1000);
+
+            } else {
+                setErrors({image: "", general: result.message});
+                setSuccessMessage("");
+            }
+
         } catch (error) {
-            console.log(error);
+            setErrors({image: "", general: error});
+        } finally {
+            setSubmitting(false);
         }
 
     };
@@ -49,12 +86,19 @@ const ProfileForm = () => {
             />
             <textarea 
                 name="bio" 
-                placeholder="Some Description" 
+                placeholder="Some description" 
+                maxLength={200}
                 required
                 value={data.bio}
                 onChange={handleChange} 
             ></textarea>
-            <button type="submit">Submit</button>
+        <p>{data.bio.length}/200</p>
+            <label htmlFor="image">Choose a profile picture:</label>
+                <input type="file" id="image" name="image" accept="image/png, image/jpeg, img/jpg, image/gif" onChange={handleChange}/>
+                {errors.image && <p className={style['error']}>{errors.image}</p>}
+            <button type="submit" disabled={submitting || errors.image !== "" || data.name.trim() === "" || data.email.trim() === "" || data.title.trim() === "" || data.bio.trim() === "" || data.image === null? true: false}>Submit</button>
+                {errors.general && <p className={style['error']}>{errors.general}</p>}
+                {successMessage && <p className={style['success']}>{successMessage}</p>}
         </form>
     );
 };
